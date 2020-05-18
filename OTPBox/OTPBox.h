11e334c69960f120c16ae2f041dc2f5c1e376a2b
tuledev <tu.leanh@digitel.com.vn>
2020-05-18 15:35:02 +0700

    Initial Commit

---
+++ OTPBox/OTPBox.h
@@ -0,0 +1,19 @@
+//
+//  OTPBox.h
+//  OTPBox
+//
+//  Created by tuledev on 5/18/20.
+//  Copyright © 2020 Digitel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OTPBox.
+FOUNDATION_EXPORT double OTPBoxVersionNumber;
+
+//! Project version string for OTPBox.
+FOUNDATION_EXPORT const unsigned char OTPBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTPBox/PublicHeader.h>
+
+
